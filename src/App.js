d800bd685ca7acb427f5f4d540d13d3f09049be9
Mohammed-100700 <salimmohammad7890@gmail.com>
2023-11-18 16:27:56 +0600

    usestate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css'
 import Navbar from './components/Navbar';
+import TestForm from './components/TestForm';
 
 function App() {
   return (
     <>
-    <Navbar title="TextUtils" aboutText='About TextUtils'/>
+      <Navbar title="TextUtils" aboutText='About TextUtils' />
+
+      <div className="container my-3">
+        <TestForm heading="Enter the text to analyze"/>
+      </div>
     </>
   );
 }
