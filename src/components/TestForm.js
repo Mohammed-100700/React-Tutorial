cfa30d12c5ffcf7cb15e42a3a954fe1d48624337
Mohammed <salimmohammad7890@gmail.com>
2023-11-19 21:39:45 +0600

    added more functions

--- src/components/TestForm.js
+++ src/components/TestForm.js
@@ -14,18 +14,38 @@ export default function TestForm(props) {
     }
 
     const handleClear = () => {
-        setText("")
+        const shouldClear = window.confirm("Are you sure you want to clear the text?");
+
+        if(shouldClear) {
+            setText("");
+        }
+    }
+
+    const handleLoClick = () => {
+        let newText = text.toLowerCase();
+        setText(newText);
     }
 
 
     return (
-        <div>
+        <>
+        <div className='container'>
             <h1>{props.heading}</h1>
             <div className="mb-3">
                 <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="10"></textarea>
             </div>
-            <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
-            <button className="btn btn-primary mx-3" onClick={handleClear}>Clear</button>
+            <button className="btn btn-dark" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className="btn btn-dark mx-3" onClick={handleLoClick}>Convert to Uppercase</button>
+            <button className="btn btn-danger" onClick={handleClear}>Clear</button>
         </div>
+
+        <div className="container my-3">
+            <h2>Your text summary</h2>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>{0.008 * text.split(" ").length} Minutes read</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
+        </>
     )
 }
