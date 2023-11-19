274df7c1381664be7860f43df4accdf9ee37a16f
Mohammed <salimmohammad7890@gmail.com>
2023-11-19 17:11:43 +0600

    hook state done

--- src/components/TestForm.js
+++ src/components/TestForm.js
@@ -2,16 +2,30 @@ import React, {useState} from 'react'
 
 export default function TestForm(props) {
 
-    const [text, setText] = useState("Enter text here");
+    const [text, setText] = useState("");
+    
+    const handleUpClick = () =>{
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+
+    const handleOnChange = (event) =>{
+        setText(event.target.value)
+    }
+
+    const handleClear = () => {
+        setText("")
+    }
 
 
     return (
         <div>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" value={text} id="myBox" rows="10"></textarea>
+                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="10"></textarea>
             </div>
-            <button className="btn btn-primary">Convert to Uppercase</button>
+            <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className="btn btn-primary mx-3" onClick={handleClear}>Clear</button>
         </div>
     )
 }
